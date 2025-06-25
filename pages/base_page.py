60aea40ba148c08bbb1078ad98af5b1e5037ae93
rivka-yuliia <juliacreadora@gmail.com>
2025-06-25 07:54:55 +0300

    is_element_present method and test the login link is present

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,11 +2,17 @@
 Base Page Object.
 """
 
+from selenium.webdriver import Chrome, Firefox
+
 
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self,
+                 browser: Chrome | Firefox,
+                 url: str,
+                 timeout: int=10) -> None:
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
--- pages/main_page.py
+++ pages/main_page.py
@@ -5,6 +5,7 @@ Page Object class for main page.
 from .base_page import BasePage
 
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 class MainPage(BasePage):
@@ -14,3 +15,14 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"),\
+            "Login link is not presented"
+
+    def is_element_present(self, locator, selector):
+        try:
+            self.browser.find_element(locator, selector)
+        except NoSuchElementException:
+            return False
+
+        return True
