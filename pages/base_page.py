57d22eee6db1013702479fbae3e39cd05c3b2596
rivka-yuliia <juliacreadora@gmail.com>
2025-06-25 07:21:07 +0300

    create BasePage class

---
+++ pages/base_page.py
@@ -0,0 +1,12 @@
+"""
+Base Page Object.
+"""
+
+
+class BasePage:
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
