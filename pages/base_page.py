3965738a330eba0670a489f88f428a1742830f55
rivka-yuliia <juliacreadora@gmail.com>
2025-06-27 07:27:50 +0300

    change class name to locators instance

--- pages/base_page.py
+++ pages/base_page.py
@@ -68,7 +68,7 @@ class BasePage:
         self.browser.get(self.url)
 
     def should_be_authorized_user(self):
-        assert self.is_element_present(*BasePageLocators.USER_ICON), \
+        assert self.is_element_present(*self.locators.USER_ICON), \
             "User icon is not presented, probably unauthorised user"
 
     def should_be_cart_link(self):
--- pages/product_page.py
+++ pages/product_page.py
@@ -12,19 +12,19 @@ class ProductPage(BasePage):
     locators = ProductPageLocators()
 
     def should_be_product_name(self):
-        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), \
+        assert self.is_element_present(*self.locators.PRODUCT_NAME), \
             "Product name not found."
 
     def should_be_product_price(self):
-        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), \
+        assert self.is_element_present(*self.locators.PRODUCT_PRICE), \
             "Product price not found."
 
     def should_be_success_message(self):
-        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+        assert self.is_element_present(*self.locators.SUCCESS_MESSAGE), \
             "Success message not found."
 
     def should_be_cart_total_message(self):
-        assert self.is_element_present(*ProductPageLocators.PRICE_MESSAGE), \
+        assert self.is_element_present(*self.locators.PRICE_MESSAGE), \
             'Total cart info message not found.'
 
     def add_to_cart(self):
@@ -34,30 +34,30 @@ class ProductPage(BasePage):
 
     def should_be_correct_product_in_success_message(self):
         msg_product_name = self.browser.find_element(
-            *ProductPageLocators.SUCCESS_MESSAGE_PRODUCT_NAME
+            *self.locators.SUCCESS_MESSAGE_PRODUCT_NAME
         ).text
         product_name = self.browser.find_element(
-            *ProductPageLocators.PRODUCT_NAME
+            *self.locators.PRODUCT_NAME
         ).text
 
         assert product_name == msg_product_name, "Product name does not match."
 
     def should_match_cart_total_msg_to_product_price(self):
         message_cart_price = self.browser.find_element(
-            *ProductPageLocators.CART_MESSAGE_PRICE
+            *self.locators.CART_MESSAGE_PRICE
         ).text
         product_price = self.browser.find_element(
-            *ProductPageLocators.PRODUCT_PRICE
+            *self.locators.PRODUCT_PRICE
         ).text
 
         assert product_price == message_cart_price, ("Cart total does not match "
                                                      "to product price.")
 
     def should_not_be_success_message(self):
-        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+        assert self.is_not_element_present(*self.locators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be."
 
     def should_disappear_success_message(self):
         time.sleep(1)
-        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+        assert self.is_disappeared(*self.locators.SUCCESS_MESSAGE), \
             "Success message is not disappeared."
